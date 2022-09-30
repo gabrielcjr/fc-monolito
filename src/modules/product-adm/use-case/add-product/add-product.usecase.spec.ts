510b5b83ffd86dd5fbe6d278b50ba2de621ecf71
Gabriel Carneiro Jr <gacarneirojr@hotmail.com>
2022-09-30 17:00:44 -0300

    criando teste para usecase

---
+++ src/modules/product-adm/use-case/add-product/add-product.usecase.spec.ts
@@ -0,0 +1,25 @@
+const MockRepository = () {
+    return {
+        add: jest.fn(),
+        find: jest.fn(),
+    }
+}
+
+describe("Add Product usecase unit test", () => {
+
+    it("should add a product", async () => {
+        const productRepository = new ProductRepository();
+        const usecase = new AddProductUseCase(productRepository);
+
+        const input = {
+            name: "Product 1",
+            description: "Product 1 description",
+            purchasePrice: 100,
+            stock: 10
+        }
+
+
+        usecase.execute(input)
+    })
+
+})
