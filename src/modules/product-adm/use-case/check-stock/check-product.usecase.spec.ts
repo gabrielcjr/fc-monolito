9706bab8f2b3193f3cd7c23430f0c77ec4fa1a2c
Gabriel Carneiro Jr <gacarneirojr@hotmail.com>
2022-11-24 15:26:09 -0300

    check-product usecase

---
+++ src/modules/product-adm/use-case/check-stock/check-product.usecase.spec.ts
@@ -0,0 +1,38 @@
+import Id from "../../../@shared/domain/value-object/id.value-object";
+import Product from "../../domain/product.entity";
+import AddProductUseCase from "../add-product/add-product.usecase";
+import CheckStockUseCase from "./check-product.usecase";
+
+const product = new Product({
+    id: new Id("1"),
+    name: "Product",
+    description: "Product description",
+    purchasePrice: 100,
+    stock: 10,
+  });
+
+const MockRepository = () => {
+    return {
+        add: jest.fn(),
+        find: jest.fn().mockReturnValue(Promise.resolve(product)),
+    }
+}
+
+describe("Check Stock usecase unit test", () => {
+
+    it("should add a product", async () => {
+        const productRepository = MockRepository();
+        let checkStockUseCase  = new CheckStockUseCase(productRepository);
+        const input = {
+            productId: "1"
+        }
+
+        const result = await checkStockUseCase.execute(input)
+
+        expect(productRepository.find).toHaveBeenCalled();
+        expect(result.productId).toBe("1");
+        expect(result.stock).toBe(10);
+        
+    })
+
+})
