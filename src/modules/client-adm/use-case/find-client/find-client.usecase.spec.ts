4a26c722c8d6d598e560a792a1be70bd3ba08dec
Gabriel Carneiro Jr <gacarneirojr@hotmail.com>
2022-12-05 12:33:26 -0300

    Implementando FindClientUseCase

---
+++ src/modules/client-adm/use-case/find-client/find-client.usecase.spec.ts
@@ -0,0 +1,36 @@
+import Id from "../../../@shared/domain/value-object/id.value-object"
+import FindClientUseCase from "./find-client.usecase"
+
+const client = {
+    id: new Id("1"),
+    name: "John Doe",
+    email: "x@x.com",
+    address: "1234 Main St",
+}
+
+const MockRepository = () => {
+    return {
+        add: jest.fn(),
+        find: jest.fn().mockReturnValue(Promise.resolve(client)),
+    }
+}
+
+describe("Find client use case", () => {
+    it("should find a client", async () => {
+        const repository = MockRepository()
+        const usecase = new FindClientUseCase(repository)
+
+        const input = {
+            id: "1",
+        }
+
+        const result = await usecase.execute(input)
+
+        expect(repository.find).toHaveBeenCalled()
+        expect (result.id).toEqual(input.id)
+        expect(result.name).toEqual(client.name)
+        expect(result.email).toEqual(client.email)
+        expect(result.address).toEqual(client.address)
+    })
+})
+
