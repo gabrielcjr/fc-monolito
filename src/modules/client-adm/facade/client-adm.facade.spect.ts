4a60caef05f62373cb0af0fc2276d83acaeba045
Gabriel Carneiro Jr <gacarneirojr@hotmail.com>
2022-12-06 15:23:33 -0300

    Implementando Add da Facade

---
+++ src/modules/client-adm/facade/client-adm.facade.spect.ts
@@ -0,0 +1,50 @@
+import { Sequelize } from "sequelize-typescript";
+import { ClientModel } from "../repository/client.model";
+import ClientRepository from "../repository/client.repository";
+import AddClientUseCase from "../use-case/add-client/add-client.usecase";
+import ClientAdmFacade from "./client-adm.facade";
+
+describe("ClientAmdFacade test", () => {
+    let sequelize: Sequelize;
+
+    beforeEach(async () => {
+        sequelize = new Sequelize({
+            dialect: "sqlite",
+            storage: ":memory:",
+            logging: false,
+            sync: { force: true },
+        });
+        await sequelize.addModels([ClientModel]);
+        await sequelize.sync();
+    })
+
+    afterEach(async () => {
+        await sequelize.close();
+    })
+    
+    it("Should create a client", async () => {
+        const repository = new ClientRepository();
+        const addUsecase = new AddClientUseCase(repository);
+        const facade = new ClientAdmFacade({
+            addUsecase: addUsecase,
+            findUsecase: undefined,
+        });
+
+        const input = {
+            id: "1",
+            name: "Client 1",
+            email: "x@x.com",
+            address: "1234 Main St",
+        }
+
+        await facade.add(input)
+
+        const client = await ClientModel.findOne({where: {id: "1"}})
+
+        expect(client).toBeDefined()
+        expect(client.id).toEqual(input.id)
+        expect(client.name).toEqual(input.name)
+        expect(client.email).toEqual(input.email)
+        expect(client.address).toEqual(input.address)
+    })
+})
---
+++ src/modules/client-adm/facade/client-adm.facade.ts
@@ -0,0 +1,27 @@
+import UseCaseInterface from "../../@shared/domain/usecase/usecase.interface";
+import ClientAdmFacadeInterface, { AddClientFacadeInputDto, FindClientFacadeInputDto, FindClientFacadeOutputDto } from "./client-adm.facade.interface";
+
+export interface UseCaseProps {
+    findUsecase?: UseCaseInterface
+    addUsecase?: UseCaseInterface
+}
+
+export default class ClientAdmFacade implements ClientAdmFacadeInterface {
+
+    private _findUsecase: UseCaseInterface
+    private _addUsecase: UseCaseInterface
+
+    constructor(props: UseCaseProps) {
+        this._findUsecase = props.findUsecase
+        this._addUsecase = props.addUsecase
+    }
+
+    async add(input: AddClientFacadeInputDto): Promise<void> {
+        await this._addUsecase.execute(input)
+    }
+
+    find(input: FindClientFacadeInputDto): Promise<FindClientFacadeOutputDto> {
+        throw new Error("Method not implemented.");
+    }
+
+}
