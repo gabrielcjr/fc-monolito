8baae474df933044e3b7824f6459b44ae4fdf491
Gabriel Carneiro Jr <gacarneirojr@hotmail.com>
2023-06-06 15:11:08 -0300

    Implementando getProduct

--- src/modules/checkout/usecase/place-order/place-order.usecase.ts
+++ src/modules/checkout/usecase/place-order/place-order.usecase.ts
@@ -1,16 +1,24 @@
 import UseCaseInterface from "../../../@shared/domain/usecase/usecase.interface";
+import Id from "../../../@shared/domain/value-object/id.value-object";
 import ClientAdmFacadeInterface from "../../../client-adm/facade/client-adm.facade.interface";
 import ProductAdmFacadeInterface from "../../../product-adm/facade/product-adm.facade.interface";
+import StoreCatalogFacadeInterface from "../../../store-catalog/facade/store-catalog.facade.interface";
+import Product from "../../domain/product.entity";
 import { PlaceOrderInputDto, PlaceOrderOutputDto } from "./place-order.dto";
 
 export default class PlaceOrderUseCase implements UseCaseInterface {
         private _clientFacade: ClientAdmFacadeInterface;
         private _productFacade: ProductAdmFacadeInterface;
+        private _catalogFacade: StoreCatalogFacadeInterface;
+
     constructor(
         clientFacade: ClientAdmFacadeInterface, 
-        productFacade: ProductAdmFacadeInterface) {
+        productFacade: ProductAdmFacadeInterface,
+        catalogFacade: StoreCatalogFacadeInterface,
+        ) {
         this._clientFacade = clientFacade;
         this._productFacade = productFacade;
+        this._catalogFacade = catalogFacade;
     }
 
     async execute(input: PlaceOrderInputDto): Promise<PlaceOrderOutputDto> {
@@ -55,5 +63,21 @@ export default class PlaceOrderUseCase implements UseCaseInterface {
                     );
             }
         }
+    }
+
+    private async getProducts(productId: string): Promise<Product> {
+        const product = await this._catalogFacade.find({id: productId});
+
+        if (!product) {
+            throw new Error(`Product not found`);
+        }
+
+        const productProps = {
+            id: new Id(product.id),
+            name: product.name,
+            description: product.description,
+            salesPrice: product.salesPrice,
+        }
+        return new Product(productProps);
     }
 }
